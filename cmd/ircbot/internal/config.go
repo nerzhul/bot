01e3b6e46178be8390849580eb22bb9cc7159ef7
Loic Blot <loic.blot@unix-experience.fr>
2018-05-16 19:59:15 +0200

    Fix IRC default bot nickname

--- cmd/ircbot/internal/config.go
+++ cmd/ircbot/internal/config.go
@@ -60,7 +60,7 @@ func (c *config) loadDefaultConfiguration() bool {
 	c.IRC.Server = "chat.freenode.net"
 	c.IRC.Port = 6697
 	c.IRC.SSL = true
-	c.IRC.Name = fmt.Sprintf("ircbot%s", time.Now().String())
+	c.IRC.Name = fmt.Sprintf("ircbot%d", time.Now().Unix())
 	return true
 }
 
